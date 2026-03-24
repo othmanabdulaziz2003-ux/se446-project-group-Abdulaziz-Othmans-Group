4f5e2ee4ea070dfc8bf656905c281ebfc792fa6d
Abdulaziz Othman <othmanabdulaziz2003@gmail.com>
2026-03-24 23:20:03 +0300

    Add Task 2: Crime Type Distribution

--- src/mapper_crime.py
+++ src/mapper_crime.py
@@ -1,1 +1,29 @@
+#!/usr/bin/env python3
+import sys
 
+# Index of relevant columns (check your CSV header!)
+# Schema: ID, Case Number, Date, ..., Arrest(8), ..., District(11)
+CRIME_TYPE_IDX = 5
+
+for line in sys.stdin:
+    line = line.strip()
+
+    # Skip empty lines
+    if not line:
+        continue
+
+    parts = line.split(',')
+
+    # Skip CSV header line
+    if parts[0] == 'ID':
+        continue
+
+    # Sanity Check: Ensure line has enough columns
+    if len(parts) <= CRIME_TYPE_IDX:
+        continue
+
+    # Extract fields
+    crime_type = parts[CRIME_TYPE_IDX]
+    print(f"{crime_type}\t1")
+
+  
