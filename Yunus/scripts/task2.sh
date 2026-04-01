cc4bdb4f146e420f2810be50add1ff69213f18a2
yunusdemirboga <135518752+yunusdemirboga@users.noreply.github.com>
2026-04-01 17:08:35 +0300

    Add Hadoop streaming script for task 2

--- Yunus/scripts/task2.sh
+++ Yunus/scripts/task2.sh
@@ -1,1 +1,11 @@
+#!/bin/bash
+source /etc/profile.d/hadoop.sh
 
+mapred streaming \
+  -files mapper_task2.py,Global_reducer.py \
+  -mapper "python3 mapper_task2.py" \
+  -reducer "python3 Global_reducer.py" \
+  -input /data/chicago_crimes.csv \
+  -output /user/ydemirboga/project/m1/task2_final
+
+hdfs dfs -cat /user/ydemirboga/project/m1/task2_final/part-00000  
